3e946c12a0332c554d0bfcacd15e745c865a15f0
Nirav Satra <nirav.satra@sakec.ac.in>
2023-07-19 15:30:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is demo for deploying React Application by Nirav</h1>
+    </div>
+  );
+}
+
+export default App;
